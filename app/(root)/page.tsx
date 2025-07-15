48d17475edf5b8a90162b78aeef2f16df387869e
Hajira-Mirza <hajiramrz@gmail.com>
2025-07-15 23:54:08 +0500

    feat: display generated interviews on the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
 import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
-import { dummyInterviews } from "@/constants";
+import {
+  getCurrentUser,
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({ userId: user?.id! }),
+  ]);
+
+  const hasPastInterviews = userInterviews?.length > 0;
+  const hasUpcomingInterviews = latestInterviews?.length > 0;
   return (
     <>
       <section className="card-cta flex flex-col-reverse items-center gap-6 p-6 md:flex-row justify-evenly">
@@ -39,24 +52,30 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Past Interviews</h2>
         <div className="interviews-section items-center justify-center">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
-          {/* <p className="text-lg">
-            You haven&apos;t scheduled any interviews yet.
-          </p> */}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p className=" text-lg">
+              You haven&apos;t scheduled any interviews yet.
+            </p>
+          )}
         </div>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Pick an Interview</h2>
         <div className="interviews-section items-center justify-center">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
-          {/* <p className="text-lg">
-            There are no interviews available.
-          </p> */}
+          {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p className="text-lg">
+              There are no upcoming interviews available.
+            </p>
+          )}
         </div>
       </section>
     </>
